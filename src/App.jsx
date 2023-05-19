694494ab00f49a87f888cc46ed184fd2193f8563
Your Name <you@example.com>
2023-05-19 16:47:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./scss/main.scss"
+
+const App = () => {
+    return (
+        <div>
+            Ihr Erfolg ist unser Erfolg
+        </div>
+    );
+};
+
+export default App;
